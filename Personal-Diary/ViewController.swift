6a26c38fddaeb547ec4e12caac20c2d993dc4597
Isaac Newton Tambunan <isaac.tambunan@ottodigital.id>
2022-11-19 23:55:37 +0700

    create base

---
+++ Personal-Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personal-Diary
+//
+//  Created by Isaac on 11/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
